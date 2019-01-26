d32496545ff4cf45b2a40cc1dff46a2706632039
Kyle Wanzek <kwanzek@gmail.com>
2019-01-25 23:13:47 -0500

    Add simple player controller and collision

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+    public int speed;
+
+    private Rigidbody2D m_Rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        m_Rigidbody2D.velocity = new Vector3(horizontalMove * speed * Time.fixedDeltaTime, verticalMove * speed * Time.fixedDeltaTime, 0);
+    }
+}
