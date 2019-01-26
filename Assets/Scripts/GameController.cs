86412dbeb097574fd9830075e002259093c2d36a
fireteamtorch <fireteam_torch@yahoo.com>
2019-01-26 13:20:04 -0500

    added logic for furniture/tile collision and attributes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public LocationController[] allLocations;
+    public FurnitureController[] allFurniture;
+
+    public float testTimer = 0f;
+    public float testThreshold = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testTimer = 0f;
+        allLocations = GameObject.FindObjectsOfType<LocationController>();
+        allFurniture = GameObject.FindObjectsOfType<FurnitureController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkAllFurnitureLocationOverlap();
+        testTimer += Time.deltaTime;
+        if(testTimer > testThreshold)
+        {
+            testTimer = 0f;
+        }
+    }
+
+    // For each furniture, check if it overlapping a specific location.
+    public void checkAllFurnitureLocationOverlap()
+    {
+        foreach (FurnitureController currFurniture in allFurniture) {
+            foreach (LocationController currLocation in allLocations)
+            {
+                if (currLocation.hasFurniture(currFurniture))
+                {
+                    currFurniture.locationBoolArray[(int)currLocation.thisLocation] = true;
+                } else
+                {
+                    currFurniture.locationBoolArray[(int)currLocation.thisLocation] = false;
+                }
+            }
+        }
+    }
+
+
+}
