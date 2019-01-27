be882a6289b8f350ecc9f2c6009deabec9ad738c
fortunac <cfortuna@bu.edu>
2019-01-26 21:26:57 -0500

    Fix timer

---
+++ Assets/Scripts/PlayerIconController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerIconController
+{
+    public PlayerController player;
+    public Text goals;
+
+    public PlayerIconController(PlayerController player)
+    {
+        this.player = player;
+        this.playerGoals
+    }
+}
